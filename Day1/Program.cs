ce4cbb82ea9b217f8a360410029f73abd6d5319b
hexesdesu <i@sxhe.xin>
2021-02-20 21:23:02 +0800

    2021年2月20日21点22分

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -7,7 +7,37 @@ namespace Day1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.ReadLine();
+            Person p = new Person();
+            Console.WriteLine(p.ToString());
+            Console.ReadKey();
+        }
+        
+        public class Person
+        {
+            public void CHLASS()
+            {
+                Console.WriteLine("吃喝拉撒睡");
+            }
+        }
+        public class NBAPlayer
+        {
+            public void KouLan()
+            {
+                Console.WriteLine("扣篮");
+            }
+        }
+        public class Student : Person,IKouLanable 
+        {
+            public void KouLan()
+            {
+                Console.WriteLine("同扣篮");
+            }
+        }
+
+        public interface IKouLanable
+        {
+            public void KouLan();
+
         }
     }
 }
