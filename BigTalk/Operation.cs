9a85d9cbf9119a1860a872ebf9deff24b9affa88
hexesdesu <i@sxhe.xin>
2021-02-21 14:45:16 +0800

    Operation类

---
+++ BigTalk/Operation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BigTalk
+{
+    class Operation
+    {
+        public static double GetResult(double numberA, double numberB, string operate)
+        {
+            double result = 0d;
+            switch (operate )
+            {
+                case "+":
+                    result = numberA + numberB;
+                    break;
+                case "-":
+                    result = numberA - numberB;
+                    break;
+                case "*":
+                    result = numberA * numberB;
+                    break;
+                case "/":
+                    result = numberA / numberB;
+                    break;
+            }
+            return result;
+        }
+    }
+}
--- BigTalk/Program.cs
+++ BigTalk/Program.cs
@@ -6,22 +6,24 @@ namespace BigTalk
     {
         static void Main(string[] args)
         {
-            Console.Write("请输入数字A:");
-            string A = Console.ReadLine();
-            Console.Write("请选择运算符号（+ - * /)");
-            string B = Console.ReadLine();
-            Console.Write("请输入数字B:");
-            string C = Console.ReadLine();
-            string D = "";
-            if (B == "+")
-                D = Convert.ToString(Convert.ToDouble(A) + Convert.ToDouble(C));
-            if (B == "-")
-                D = Convert.ToString(Convert.ToDouble(A) - Convert.ToDouble(C));
-            if (B == "*")
-                D = Convert.ToString(Convert.ToDouble(A) * Convert.ToDouble(C));
-            if (B == "/")
-                D = Convert.ToString(Convert.ToDouble(A) / Convert.ToDouble(C));
-            Console.WriteLine("结果是：" + D);
+            try
+            {
+                Console.Write("A:");
+                string strNumberA = Console.ReadLine();
+                Console.Write("+-*/");
+                string strOperate = Console.ReadLine();
+                Console.Write("B:");
+                string strNumberB = Console.ReadLine();
+                string strResult = "";
+                strResult = Convert.ToString(Operation.GetResult(Convert.ToDouble(strNumberA),
+                    Convert.ToDouble(strNumberB), strOperate));
+                Console.WriteLine("Result:" + strResult);
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Something Wrong!" + ex.Message);
+            }
 
         }
     }
