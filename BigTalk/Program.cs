213dba623fd3491e7382999e8e87dafd7b99808a
hexesdesu <i@sxhe.xin>
2021-02-21 14:29:47 +0800

    初学者代码

---
+++ BigTalk/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BigTalk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("请输入数字A:");
+            string A = Console.ReadLine();
+            Console.Write("请选择运算符号（+ - * /)");
+            string B = Console.ReadLine();
+            Console.Write("请输入数字B:");
+            string C = Console.ReadLine();
+            string D = "";
+            if (B == "+")
+                D = Convert.ToString(Convert.ToDouble(A) + Convert.ToDouble(C));
+            if (B == "-")
+                D = Convert.ToString(Convert.ToDouble(A) - Convert.ToDouble(C));
+            if (B == "*")
+                D = Convert.ToString(Convert.ToDouble(A) * Convert.ToDouble(C));
+            if (B == "/")
+                D = Convert.ToString(Convert.ToDouble(A) / Convert.ToDouble(C));
+            Console.WriteLine("结果是：" + D);
+
+        }
+    }
+}
