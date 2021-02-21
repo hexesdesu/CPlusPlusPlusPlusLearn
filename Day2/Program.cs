722373dbeaafe2ebd78a2c69c7b06c652fb5aa4e
hexesdesu <i@sxhe.xin>
2021-02-21 09:02:49 +0800

    新增Day2

---
+++ Day2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ISwimming swim = new XPDuck();
+            swim.Swim();
+            Console.ReadKey();
+        }
+    }
+    public class RealDuck : ISwimming
+    {
+        public void Swim()
+        {
+            Console.WriteLine("真的鸭子会游泳");
+        }
+    }
+
+    public class MuDuck : RealDuck
+    {
+
+    }
+
+    public class XPDuck : ISwimming
+    {
+        public void Swim()
+        {
+            Console.WriteLine("飘着游泳");
+        }
+    }
+
+    public interface ISwimming
+    {
+        void Swim();
+    }
+}
