cc9ece8bda9d4905f3bc8c4eb3cd0ac7ca143e10
Hex Jerryson <i@sxhe.xin>
2021-02-23 19:35:58 +0800

    雷锋工厂

--- Day4/BigTalk/Program.cs
+++ Day4/BigTalk/Program.cs
@@ -6,8 +6,45 @@ namespace BigTalk
     {
         static void Main(string[] args)
         {
+            /* 代理模式
             Proxy proxy = new Proxy();
             proxy.Request();
+            **/
+
+            /* 雷锋简单实现
+            LeiFeng xueleifeng = new Undergraduate();
+            xueleifeng.BuyRice();
+            xueleifeng.Sweep();
+            xueleifeng.Wash();
+            **/
+
+            /* 雷锋替代简单实现，但学生需要毕业
+            LeiFeng student1 = new Undergraduate();
+            student1.BuyRice();
+            LeiFeng student2 = new Undergraduate();
+            student1.Sweep();
+            LeiFeng student3 = new Undergraduate();
+            student1.Wash();
+            **/
+
+            /* 雷锋学生替代简单实现，有重复代码
+            LeiFeng studentA = SimpleFactory.CreateLeiFeng("学雷锋的大学生");
+            studentA.BuyRice();
+            LeiFeng studentB = SimpleFactory.CreateLeiFeng("学雷锋的大学生");
+            studentA.Sweep();
+            LeiFeng studentC = SimpleFactory.CreateLeiFeng("学雷锋的大学生");
+            studentA.Wash();
+            **/
+
+            //雷锋工厂
+            IFactory factory = new UnderGraduateFactory();
+            LeiFeng student = factory.CreateLeiFeng();
+
+            student.BuyRice();
+            student.Sweep();
+            student.Wash();
+
+
         }
     }
 }
---
+++ Day4/BigTalk/factory.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BigTalk
+{
+    
+    class LeiFeng
+    {
+        public void Sweep()
+        {
+            Console.WriteLine("扫地");
+        }
+        public void Wash()
+        {
+            Console.WriteLine("洗衣");
+        }
+        public void BuyRice()
+        {
+            Console.WriteLine("买米");
+        }
+    }
+    class Undergraduate : LeiFeng 
+    { }
+
+    class Volunteer : LeiFeng
+    { }
+
+    class SimpleFactory
+    {
+        public static LeiFeng CreateLeiFeng(string type)
+        {
+            LeiFeng result = null;
+            switch (type)
+            {
+                case "学雷锋的大学生":
+                    result = new Undergraduate();
+                    break;
+                case "社区志愿者":
+                    result = new Undergraduate();
+                    break;
+            }
+            return result;
+        }
+    }
+    interface IFactory
+    {
+        LeiFeng CreateLeiFeng();
+    }
+    class UnderGraduateFactory : IFactory
+    {
+        public LeiFeng CreateLeiFeng()
+        {
+            return new Undergraduate();
+        }
+    }
+
+    class VolunteerFactory : IFactory
+    {
+        public LeiFeng CreateLeiFeng()
+        {
+            return new Volunteer();
+        }
+    }
+
+}
