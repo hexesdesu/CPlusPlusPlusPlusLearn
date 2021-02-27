7d2d5ddaf0b3addfa76202172b7c4927175af388
Hex Jerryson <i@sxhe.xin>
2021-02-27 14:48:34 +0800

    建造者模式基本代码

---
+++ Day7/BigTalk/product.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigTalk
+{
+    class Product
+    {
+        IList<string> parts = new List<string>();
+
+        public void Add(string part)
+        {
+            parts.Add(part);
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("\n Product Build ----");
+            foreach (string part in parts)
+            {
+                Console.WriteLine(part);
+            }
+        }
+    }
+
+    abstract class Builder
+    {
+        public abstract void BuildPartA();
+        public abstract void BuildPartB();
+        public abstract Product GetResult();
+    }
+
+    class ConcreteBuilder1 : Builder
+    {
+        private Product product = new Product();
+
+        public override void BuildPartA()
+        {
+            product.Add("Part A");
+        }
+
+        public override void BuildPartB()
+        {
+            product.Add("Part B");
+        }
+
+        public override Product GetResult()
+        {
+            return product;
+        }
+    }
+
+    class ConcreteBuilder2 : Builder
+    {
+        private Product product = new Product();
+
+        public override void BuildPartA()
+        {
+            product.Add("Part X");
+        }
+
+        public override void BuildPartB()
+        {
+            product.Add("Part Y");
+        }
+
+        public override Product GetResult()
+        {
+            return product;
+        }
+    }
+    class Director
+    {
+        public void Construct(Builder builder)
+        {
+            builder.BuildPartA();
+            builder.BuildPartB();
+        }
+    }
+}
