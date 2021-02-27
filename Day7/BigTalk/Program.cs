35cb0311263f3972af22ce18a7c16b451d38aefc
Hex Jerryson <i@sxhe.xin>
2021-02-27 15:21:13 +0800

    观察者模式第一版

--- Day7/BigTalk/Program.cs
+++ Day7/BigTalk/Program.cs
@@ -10,17 +10,31 @@ namespace BigTalk
     {
         static void Main(string[] args)
         {
-            Director director = new Director();
-            Builder b1 = new ConcreteBuilder1();
-            Builder b2 = new ConcreteBuilder2();
+            /*建造者模式
+           Director director = new Director();
+           Builder b1 = new ConcreteBuilder1();
+           Builder b2 = new ConcreteBuilder2();
 
-            director.Construct(b1);
-            Product p1 = b1.GetResult();
-            p1.Show();
+           director.Construct(b1);
+           Product p1 = b1.GetResult();
+           p1.Show();
 
-            director.Construct(b2);
-            Product p2 = b2.GetResult();
-            p2.Show();
+           director.Construct(b2);
+           Product p2 = b2.GetResult();
+           p2.Show();
+           **/
+
+            Secretary tongzizhe = new Secretary();
+
+            StockObserver tongshi1 = new StockObserver("weiguancha", tongzizhe);
+            StockObserver tongshi2 = new StockObserver("yiguancha", tongzizhe);
+
+            tongzizhe.Attach(tongshi1);
+            tongzizhe.Attach(tongshi2);
+
+            tongzizhe.SecretaryAction = "Boss!";
+
+            tongzizhe.Notiry();
 
             Console.Read();
         }
---
+++ Day7/BigTalk/watch.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigTalk
+{
+    class Secretary
+    {
+        private IList<StockObserver> observers = new List<StockObserver>();
+        private string action;
+
+        public void Attach(StockObserver observer)
+        {
+            observers.Add(observer);
+        }
+
+        public void Notiry()
+        {
+            foreach (StockObserver o in observers)
+                o.Update();
+        }
+
+        public string SecretaryAction
+        {
+            get { return action; }
+            set { action = value; }
+        }
+    }
+
+    class StockObserver
+    {
+        private string name;
+        private Secretary sub;
+        public StockObserver (string name,Secretary sub)
+        {
+            this.name = name;
+            this.sub = sub;
+        }
+        public void Update()
+        {
+            Console.WriteLine("{0}{1}关闭股票行情，继续工作", sub.SecretaryAction, name);
+        }
+    }
+}
