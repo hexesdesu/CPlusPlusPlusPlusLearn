c7a4986d02c165b849b463191ff95e83d0e3f8d0
hexesdesu <i@sxhe.xin>
2021-02-28 10:25:21 +0800

    Day8

---
+++ Day8/BigTalk/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BigTalk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Secretary tongzizhe = new Secretary();
+
+            StockObserver tongshi1 = new StockObserver("weiguancha", tongzizhe);
+            StockObserver tongshi2 = new StockObserver("yiguancha", tongzizhe);
+
+            tongzizhe.Attach(tongshi1);
+            tongzizhe.Attach(tongshi2);
+
+            tongzizhe.SecretaryAction = "Boss!";
+
+            tongzizhe.Notiry();
+
+            Console.Read();
+        }
+    }
+}
---
+++ Day8/BigTalk/watch.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BigTalk
+{
+    class Secretary
+    {
+        private IList<StockObserver> observers = new List<StockObserver>();
+        private string action;
+
+        public void Attach(StockObserver observer)
+        {
+            observers.Add(observer);
+        }
+
+        public void Notiry()
+        {
+            foreach (StockObserver o in observers)
+                o.Update();
+        }
+
+        public string SecretaryAction
+        {
+            get { return action; }
+            set { action = value; }
+        }
+    }
+
+    class StockObserver
+    {
+        private string name;
+        private Secretary sub;
+        public StockObserver(string name, Secretary sub)
+        {
+            this.name = name;
+            this.sub = sub;
+        }
+        public void Update()
+        {
+            Console.WriteLine("{0}{1}关闭股票行情，继续工作", sub.SecretaryAction, name);
+        }
+    }
+}
