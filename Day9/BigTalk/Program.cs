50a5baf93be9ed74dc0f6163a651479cbb9191f6
hexesdesu <i@sxhe.xin>
2021-03-01 08:28:01 +0800

    观察者模式

--- Day9/BigTalk/Program.cs
+++ Day9/BigTalk/Program.cs
@@ -6,7 +6,17 @@ namespace BigTalk
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ConcreteSubject s = new ConcreteSubject();
+
+            s.Attach(new ConcreteObserver(s, "X"));
+            s.Attach(new ConcreteObserver(s, "Y"));
+            s.Attach(new ConcreteObserver(s, "Z"));
+
+            s.SubjectState = "ABC";
+
+            s.Notify();
+
+            Console.Read();
         }
     }
 }
---
+++ Day9/BigTalk/watch.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BigTalk
+{
+    abstract class Subject
+    {
+        private IList<Observer> observers = new List<Observer>();
+        public void Attach(Observer observer) => observers.Add(observer);
+        public void Detach(Observer observer) => observers.Remove(observer);
+        public void Notify()
+        {
+            foreach (var o in observers)
+                o.Update();
+        }
+    }
+
+    abstract class Observer
+    {
+        public abstract void Update();
+    }
+
+    class ConcreteSubject : Subject
+    {
+        public string SubjectState { get; set; }
+    }
+
+    class ConcreteObserver : Observer
+    {
+        private string name;
+        private string observerState;
+        public ConcreteSubject Subject { get; set; }
+        public ConcreteObserver(ConcreteSubject subject,string name)
+        {
+            Subject = subject;
+            this.name = name;
+        }
+        public override void Update()
+        {
+            observerState = Subject.SubjectState;
+            Console.WriteLine($"Observer {name}'s new state is {observerState}");
+        }
+    }
+}
