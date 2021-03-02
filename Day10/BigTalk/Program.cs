3145e5dfc5d934c335682b7ecfbb16c1a71b94b6
hexesdesu <i@sxhe.xin>
2021-03-02 08:39:44 +0800

    Day10

---
+++ Day10/BigTalk/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BigTalk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+            SqlserverUser su = new SqlserverUser();
+            su.Insert(user);
+            su.GetUser(1);
+
+            Console.Read();
+        }
+    }
+}
---
+++ Day10/BigTalk/user.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BigTalk
+{
+    class User
+    {
+        private int _id;
+        public int ID
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+    }
+
+    class SqlserverUser
+    {
+        public void Insert(User user)
+        {
+            Console.WriteLine("在SQLServer中给User表增加一条记录");
+        }
+
+        public User GetUser(int id)
+        {
+            Console.WriteLine("在SQLServer中根据ID得到User表一条记录");
+            return null;
+        }
+    }
+}
