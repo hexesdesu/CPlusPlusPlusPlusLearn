0c38ece774cd60db27fb3551ce092c4a7ce7f21a
hexesdesu <i@sxhe.xin>
2021-03-03 10:29:30 +0800

    适配器模式

--- Day11/BigTalk/Program.cs
+++ Day11/BigTalk/Program.cs
@@ -6,27 +6,8 @@ namespace BigTalk
     {
         static void Main(string[] args)
         {
-            Work emergencyProjects = new Work();
-            emergencyProjects.Hour = 9;
-            emergencyProjects.WriteProgram();
-            emergencyProjects.Hour = 10;
-            emergencyProjects.WriteProgram();
-            emergencyProjects.Hour = 12;
-            emergencyProjects.WriteProgram();
-            emergencyProjects.Hour = 13;
-            emergencyProjects.WriteProgram();
-            emergencyProjects.Hour = 14;
-            emergencyProjects.WriteProgram();
-            emergencyProjects.Hour = 17;
-
-            emergencyProjects.TaskFinished = true;
-
-            emergencyProjects.WriteProgram();
-            emergencyProjects.Hour = 19;
-            emergencyProjects.WriteProgram();
-            emergencyProjects.Hour = 22;
-            emergencyProjects.WriteProgram();
-
+            Target target = new Adapter();
+            target.Reqest();
 
             Console.Read();
         }
---
+++ Day11/BigTalk/adapter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BigTalk
+{
+    class Target
+    {
+        public virtual void Reqest()
+        {
+            Console.WriteLine("普通请求！");
+        }
+    }
+
+    class Adaptee
+    {
+        public void SpecificRequest()
+        {
+            Console.WriteLine("特殊请求");
+        }
+    }
+
+    class Adapter : Target
+    {
+        private Adaptee adaptee = new Adaptee();
+        public override void Reqest()
+        {
+            adaptee.SpecificRequest();
+        }
+    }
+
+}
