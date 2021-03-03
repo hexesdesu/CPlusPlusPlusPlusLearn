9026af9152928b80137de13d4a63b31584383998
hexesdesu <i@sxhe.xin>
2021-03-03 09:23:57 +0800

    状态模式

--- Day11/BigTalk/Program.cs
+++ Day11/BigTalk/Program.cs
@@ -6,27 +6,12 @@ namespace BigTalk
     {
         static void Main(string[] args)
         {
-            Work emergencyProject = new Work();
-            emergencyProject.Hour = 9;
-            emergencyProject.WriteProgram();
-            emergencyProject.Hour = 10;
-            emergencyProject.WriteProgram();
-            emergencyProject.Hour = 12;
-            emergencyProject.WriteProgram();
-            emergencyProject.Hour = 13;
-            emergencyProject.WriteProgram();
-            emergencyProject.Hour = 14;
-            emergencyProject.WriteProgram();
-            emergencyProject.Hour = 17;
-            emergencyProject.WriteProgram();
+            Context c = new Context(new ConcreteStateA());
 
-            emergencyProject.TaskFinished = false;
-
-            emergencyProject.WriteProgram();
-            emergencyProject.Hour = 19;
-            emergencyProject.WriteProgram();
-            emergencyProject.Hour = 22;
-            emergencyProject.WriteProgram();
+            c.Request();
+            c.Request();
+            c.Request();
+            c.Request();
 
             Console.Read();
         }
---
+++ Day11/BigTalk/state.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BigTalk
+{
+    abstract class State
+    {
+        public abstract void Handle(Context context);
+    }
+
+    class ConcreteStateA : State
+    {
+        public override void Handle(Context context)
+        {
+            context.State = new ConcreteStateB();
+        }
+    }
+
+    class ConcreteStateB : State
+    {
+        public override void Handle(Context context)
+        {
+            context.State = new ConcreteStateA();
+        }
+    }
+
+    class Context
+    {
+        private State state;
+        public Context(State state)
+        {
+            this.state = state;
+        }
+        public State State
+        {
+            get { return state; }
+            set
+            {
+                state = value;
+                Console.WriteLine("当前状态：" + state.GetType().Name);
+            }
+        }
+        public void Request()
+        {
+            state.Handle(this);
+        }
+    }
+}
