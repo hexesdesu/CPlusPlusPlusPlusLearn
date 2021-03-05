bd5143558a9c20473633d3a394e25d63c73cc7e0
hexesdesu <i@sxhe.xin>
2021-03-05 20:55:10 +0800

    职责链模式（part）

---
+++ Day12/BigTalk/request.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BigTalk
+{
+    class Request
+    {
+        private string requestType;
+        public string RequestType
+        {
+            get { return requestType; }
+            set { requestType = value; }
+        }
+        private string requestContent;
+        public string RequestConteng
+        {
+            get { return requestContent; }
+            set { requestContent = value; }
+        }
+        private int number;
+        public int Number
+        {
+            get { return number; }
+            set { number = value; }
+        }
+    }
+
+    /*class Manager
+    {
+        protected string name;
+        public Manager(string name) { this.name = name; }
+        public 
+    }*/
+}
