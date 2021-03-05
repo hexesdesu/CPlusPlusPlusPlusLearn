60ae060a4f21a146dada70c69869b866047b03b4
hexesdesu <i@sxhe.xin>
2021-03-05 16:30:21 +0800

    松耦合烤串

--- Day12/BigTalk/Program.cs
+++ Day12/BigTalk/Program.cs
@@ -6,14 +6,27 @@ namespace BigTalk
     {
         static void Main(string[] args)
         {
+            /*Barbecuer boy = new Barbecuer();
+            boy.BakeMutton();
+            boy.BakeMutton();
+            boy.BakeMutton();
+            boy.BakeChickenWing();
+            boy.BakeMutton();
+            boy.BakeMutton();
+            boy.BakeChickenWing();*/
+
             Barbecuer boy = new Barbecuer();
-            boy.BakeMutton();
-            boy.BakeMutton();
-            boy.BakeMutton();
-            boy.BakeChickenWing();
-            boy.BakeMutton();
-            boy.BakeMutton();
-            boy.BakeChickenWing();
+            Command bakeMuttonCommand1 = new BakeMuttonCommand(boy);
+            Command bakeMuttonCommand2 = new BakeMuttonCommand(boy);
+            Command bakeChickenWingCommad1 = new BakeChickenWingCommand(boy);
+            Waiter girl = new Waiter();
+
+            girl.SetOrder(bakeMuttonCommand1);
+            girl.Notify();
+            girl.SetOrder(bakeMuttonCommand2);
+            girl.Notify();
+            girl.SetOrder(bakeChickenWingCommad1);
+            girl.Notify();
 
             Console.Read();
         }
--- Day12/BigTalk/barbebu.cs
+++ Day12/BigTalk/barbebu.cs
@@ -15,4 +15,45 @@ namespace BigTalk
             Console.WriteLine("烤鸡翅！");
         }
     }
+
+    public abstract class Command
+    {
+        protected Barbecuer receiver;
+        public Command(Barbecuer receiver)
+        {
+            this.receiver = receiver;
+        }
+        abstract public void ExcuteCommmand();
+    }
+
+    class BakeMuttonCommand : Command
+    {
+        public BakeMuttonCommand(Barbecuer receiver) : base(receiver) { }
+        public override void ExcuteCommmand()
+        {
+            receiver.BakeMutton();
+        }
+    }
+
+    class BakeChickenWingCommand : Command
+    {
+        public BakeChickenWingCommand(Barbecuer receiver) : base(receiver) { }
+        public override void ExcuteCommmand()
+        {
+            receiver.BakeChickenWing();
+        }
+    }
+
+    public class Waiter
+    {
+        private Command command;
+        public void SetOrder(Command command)
+        {
+            this.command = command;
+        }
+        public void Notify()
+        {
+            command.ExcuteCommmand();
+        }
+    }
 }
