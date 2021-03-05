446b052fb3f687ef9e5b7c22b8137ab7e7a4febd
hexesdesu <i@sxhe.xin>
2021-03-05 08:21:48 +0800

    单例模式

--- Day12/BigTalk/Program.cs
+++ Day12/BigTalk/Program.cs
@@ -6,7 +6,15 @@ namespace BigTalk
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("两个对象是相同的实例");
+            }
+
+            Console.Read();
         }
     }
 }
---
+++ Day12/BigTalk/singleton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BigTalk
+{
+    class Singleton
+    {
+        private static Singleton instance;
+        private Singleton() { }
+        public static Singleton GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+            return instance;
+        }
+    }
+}
