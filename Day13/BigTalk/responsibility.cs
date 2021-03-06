838ac2f24b37d3942831862d360e4c2456eb1f24
hexesdesu <i@sxhe.xin>
2021-03-06 10:43:12 +0800

    职责链

---
+++ Day13/BigTalk/responsibility.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigTalk
+{
+    abstract class Handler
+    {
+        protected Handler successor;
+        public void SetSuccessor(Handler successor)
+        {
+            this.successor = successor;
+        }
+        public abstract void HandleRequest(int request);
+    }
+
+    class ConcreteHandler1 : Handler
+    {
+        public override void HandleRequest(int request)
+        {
+            if(request >=0 && request < 10)
+            {
+                Console.WriteLine("{0}请求处理{1}", this.GetType().Name, request);
+            }
+            else if (successor != null)
+            {
+                successor.HandleRequest(request);
+            }
+        }
+    }
+
+    class ConcreteHandler2 : Handler
+    {
+        public override void HandleRequest(int request)
+        {
+            if (request >= 10 && request < 20)
+            {
+                Console.WriteLine("{0}请求处理{1}", this.GetType().Name, request);
+            }
+            else if (successor != null)
+            {
+                successor.HandleRequest(request);
+            }
+        }
+    }
+
+    class ConcreteHandler3 : Handler
+    {
+        public override void HandleRequest(int request)
+        {
+            if (request >= 20 && request < 30)
+            {
+                Console.WriteLine("{0}请求处理{1}", this.GetType().Name, request);
+            }
+            else if (successor != null)
+            {
+                successor.HandleRequest(request);
+            }
+        }
+    }
+}
