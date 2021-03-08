d5c05e8d2f7a6e1645a5f64f2315da7e39ceb9e2
hexesdesu <i@sxhe.xin>
2021-03-08 14:09:04 +0800

    享元模式 字符串string

--- Day15/BigTalk/Program.cs
+++ Day15/BigTalk/Program.cs
@@ -6,7 +6,7 @@ namespace BigTalk
     {
         static void Main(string[] args)
         {
-            WebSiteFactory f = new WebSiteFactory();
+            /*WebSiteFactory f = new WebSiteFactory();
 
             WebSite fx = f.GetWebSiteCategory("产品展示");
             fx.Use(new User("小菜"));
@@ -21,7 +21,12 @@ namespace BigTalk
             WebSite fn = f.GetWebSiteCategory("博客");
             fn.Use(new User("南海鳄神"));
 
-            Console.WriteLine("得到的网站分类总数为 {0}", f.GetWebsiteCount());
+            Console.WriteLine("得到的网站分类总数为 {0}", f.GetWebsiteCount());*/
+
+            string titleA = "大话设计模式";
+            string titleB = "大话设计模";
+            titleB = "大话设计模式";
+            Console.WriteLine(Object.ReferenceEquals(titleA, titleB));
 
             Console.Read();
         }
