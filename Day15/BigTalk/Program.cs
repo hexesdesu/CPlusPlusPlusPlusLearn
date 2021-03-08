9dc2f2530039e440fcc2d5fc23f923d31da2b9ef
hexesdesu <i@sxhe.xin>
2021-03-08 11:09:38 +0800

    享元模式 网站

--- Day15/BigTalk/Program.cs
+++ Day15/BigTalk/Program.cs
@@ -6,7 +6,24 @@ namespace BigTalk
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WebSiteFactory f = new WebSiteFactory();
+
+            WebSite fx = f.GetWebSiteCategory("产品展示");
+            fx.Use(new User("小菜"));
+            WebSite fy = f.GetWebSiteCategory("产品展示");
+            fy.Use(new User("大鸟"));
+            WebSite fz = f.GetWebSiteCategory("产品展示");
+            fz.Use(new User("娇娇"));
+            WebSite f1 = f.GetWebSiteCategory("博客");
+            f1.Use(new User("老顽童"));
+            WebSite fm = f.GetWebSiteCategory("博客");
+            fm.Use(new User("桃谷六仙"));
+            WebSite fn = f.GetWebSiteCategory("博客");
+            fn.Use(new User("南海鳄神"));
+
+            Console.WriteLine("得到的网站分类总数为 {0}", f.GetWebsiteCount());
+
+            Console.Read();
         }
     }
 }
