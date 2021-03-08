1c2e4516f92ce9b53d6d672a860bf982f3afafc5
hexesdesu <i@sxhe.xin>
2021-03-08 09:47:25 +0800

    享元模式 网站

--- Day14/BigTalk/Program.cs
+++ Day14/BigTalk/Program.cs
@@ -6,7 +6,7 @@ namespace BigTalk
     {
         static void Main(string[] args)
         {
-            int extrinsicstate = 22;
+            /*int extrinsicstate = 22;
 
             FlyweightFactory f = new FlyweightFactory();
 
@@ -21,7 +21,24 @@ namespace BigTalk
 
             Flyweight uf = new UnsharedConcreteFlyweight();
 
-            uf.Operation(--extrinsicstate);
+            uf.Operation(--extrinsicstate);*/
+
+            WebsiteFactory f = new WebsiteFactory();
+
+            WebSite fx = f.GetWebsiteCategory("产品展示");
+            fx.Use();
+            WebSite fy = f.GetWebsiteCategory("产品展示");
+            fy.Use();
+            WebSite fz = f.GetWebsiteCategory("产品展示");
+            fz.Use();
+            WebSite f1 = f.GetWebsiteCategory("博客");
+            f1.Use();
+            WebSite fm = f.GetWebsiteCategory("博客");
+            fm.Use();
+            WebSite fn = f.GetWebsiteCategory("博客");
+            fn.Use();
+
+            Console.WriteLine("网站分类总数为：{0}", f.GetWebsiteCount());
 
             Console.Read();
         }
---
+++ Day14/BigTalk/website.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigTalk
+{
+    abstract class WebSite
+    {
+        public abstract void Use();
+    }
+
+    class ConcreteWebSite : WebSite
+    {
+        private string name = "";
+        public ConcreteWebSite(string name)
+        {
+            this.name = name;
+        }
+        public override void Use()
+        {
+            Console.WriteLine("网站分类：" + name);
+        }
+    }
+
+    class WebsiteFactory
+    {
+        private Hashtable flyweithts = new Hashtable();
+        public WebSite GetWebsiteCategory(string key)
+        {
+            if (!flyweithts.ContainsKey(key))
+                flyweithts.Add(key, new ConcreteWebSite(key));
+            return ((WebSite)flyweithts[key]);
+        }
+        public int GetWebsiteCount()
+        {
+            return flyweithts.Count;
+        }
+    }
+}
