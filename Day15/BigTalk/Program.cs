59b32c5eeb46da5b4bff7cb5f6f76443d366499a
hexesdesu <i@sxhe.xin>
2021-03-08 14:21:48 +0800

    解释器模式

--- Day15/BigTalk/Program.cs
+++ Day15/BigTalk/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BigTalk
 {
@@ -6,27 +7,17 @@ namespace BigTalk
     {
         static void Main(string[] args)
         {
-            /*WebSiteFactory f = new WebSiteFactory();
+            Context context = new Context();
+            IList<AbstractExpression> list = new List<AbstractExpression>();
+            list.Add(new TerminalExpression());
+            list.Add(new NonterminalExpression());
+            list.Add(new TerminalExpression());
+            list.Add(new TerminalExpression());
 
-            WebSite fx = f.GetWebSiteCategory("产品展示");
-            fx.Use(new User("小菜"));
-            WebSite fy = f.GetWebSiteCategory("产品展示");
-            fy.Use(new User("大鸟"));
-            WebSite fz = f.GetWebSiteCategory("产品展示");
-            fz.Use(new User("娇娇"));
-            WebSite f1 = f.GetWebSiteCategory("博客");
-            f1.Use(new User("老顽童"));
-            WebSite fm = f.GetWebSiteCategory("博客");
-            fm.Use(new User("桃谷六仙"));
-            WebSite fn = f.GetWebSiteCategory("博客");
-            fn.Use(new User("南海鳄神"));
-
-            Console.WriteLine("得到的网站分类总数为 {0}", f.GetWebsiteCount());*/
-
-            string titleA = "大话设计模式";
-            string titleB = "大话设计模";
-            titleB = "大话设计模式";
-            Console.WriteLine(Object.ReferenceEquals(titleA, titleB));
+            foreach(AbstractExpression exp in list)
+            {
+                exp.Interpret(context);
+            }
 
             Console.Read();
         }
---
+++ Day15/BigTalk/interpreter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigTalk
+{
+    abstract class AbstractExpression
+    {
+        public abstract void Interpret(Context context);
+    }
+
+    class TerminalExpression: AbstractExpression
+    {
+        public override void Interpret(Context context)
+        {
+            Console.WriteLine("终端解释器");
+        }
+    }
+
+    class NonterminalExpression : AbstractExpression
+    {
+        public override void Interpret(Context context)
+        {
+            Console.WriteLine("非终端解释器");
+        }
+    }
+
+    class Context
+    {
+        private string input;
+        public string Input
+        {
+            get { return input; }
+            set { input = value; }
+        }
+        private string output;
+        public string Output
+        {
+            get { return output; }
+            set { output = value; }
+        }
+    }
+}
