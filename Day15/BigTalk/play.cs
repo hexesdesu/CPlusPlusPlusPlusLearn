266b684c70cf3c64b7902e2ee28aa779dfc3dfb4
hexesdesu <i@sxhe.xin>
2021-03-09 08:03:36 +0800

    音乐解释器 part

---
+++ Day15/BigTalk/play.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigTalk
+{
+    class PlayContext
+    {
+        private string text;
+        public string PlayText
+        {
+            get { return text; }
+            set { text = value; }
+        }
+    }
+
+    abstract class Expression
+    {
+
+    }
+}
