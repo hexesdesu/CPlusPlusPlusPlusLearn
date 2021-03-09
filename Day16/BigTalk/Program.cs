f9f213bb6b6605b78ea9f9ec13b265a803f6964e
hexesdesu <i@sxhe.xin>
2021-03-09 08:25:08 +0800

    Day16(part)

---
+++ Day16/BigTalk/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace BigTalk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PlayContext context = new PlayContext();
+            Console.WriteLine("上海滩：");
+            context.PlayText = "O 2 E 0.5 G 0.5 A 3 E 0.5 G 0.5 D 3 E 0.5 G 0.5 A 0 3 C 1 0 2 A 0.5 G 1 C 0.5 E 0.5 D 3";
+            Expression expression = null;
+            try
+            {
+                while (context.PlayText.Length > 0)
+                {
+                    string str = context.PlayText.Substring(0, 1);
+                    switch (str)
+                    {
+                        case "O":
+                            expression = new Scale();
+                            break;
+                        case "C":
+                        case "D":
+                        case "E":
+                        case "F":
+                        case "G":
+                        case "A":
+                        case "B":
+                        case "P":
+                            expression = new Note();
+                            break;
+                    }
+                    expression.Interpret(context);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.Read();
+        }
+    }
+}
