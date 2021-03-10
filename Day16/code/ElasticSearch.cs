cd9700021ef52b75f5c728692e58e02a2b7cffe2
hexesdesu <i@sxhe.xin>
2021-03-10 08:03:44 +0800

    尝试优化代码

---
+++ Day16/code/ElasticSearch.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace code
+{
+    abstract class GenericElasticSearch
+    {
+        public abstract void Handling();
+    }
+
+    class ElasticSearch : GenericElasticSearch
+    {
+        public override void Handling()
+        {
+            Console.WriteLine("处理ElasticSearch");
+        }
+    }
+
+    class TemplateElasticSearch : GenericElasticSearch
+    {
+        public override void Handling()
+        {
+            Console.WriteLine("处理TemplateElasticSearch");
+        }
+    }
+
+    class TaskGroupElasticSearch : GenericElasticSearch
+    {
+        public override void Handling()
+        {
+            Console.WriteLine("处理TaskGroupElasticSearch");
+        }
+    }
+
+    class EventsGroupElasticSearch : GenericElasticSearch
+    {
+        public override void Handling()
+        {
+            Console.WriteLine("处理EventsGroupElasticSearch");
+        }
+    }
+
+    class StudentInfoElasticSearch : GenericElasticSearch
+    {
+        public override void Handling()
+        {
+            Console.WriteLine("处理StudentInfoElasticSearch");
+        }
+    }
+}
---
+++ Day16/code/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SearchTypeEnum searchTypeEnum = new SearchTypeEnum();
+
+            Console.WriteLine(searchTypeEnum.TransType2Class("全部"));
+
+            Console.Read();
+        }
+    }
+}
