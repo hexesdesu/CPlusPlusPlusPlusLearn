38aa3971af164a4b52f46b4f53f490a86f4445b9
hexesdesu <i@sxhe.xin>
2021-03-13 08:24:27 +0800

    简单把类做封装，需要在客户端做对应的new操作

--- Day19/BigTalk/Program.cs
+++ Day19/BigTalk/Program.cs
@@ -6,7 +6,38 @@ namespace BigTalk
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Operation o = new Add();
+            o.Num1 = 1;
+            o.Num2 = 2;
+            Console.WriteLine(o.GetResult());
+        }
+    }
+
+    public abstract class Operation
+    {
+        private double num1;
+        private double num2;
+
+        public double Num1
+        {
+            get;
+            set;
+        }
+
+        public double Num2
+        {
+            get;
+            set;
+        }
+
+        public abstract double GetResult();
+    }
+
+    public class Add : Operation
+    {
+        public override double GetResult()
+        {
+            return this.Num1 + this.Num2;
         }
     }
 }
